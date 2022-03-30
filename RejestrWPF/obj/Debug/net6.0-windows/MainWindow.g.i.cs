8ccacc2d5e2965ab9d7ed3fe025ba2c146d6f49c
David Kuc <david.kuc@microsoft.wsei.edu.pl>
2022-03-30 09:51:49 +0200

    qc

--- RejestrWPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ RejestrWPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -125,7 +125,7 @@ namespace RejestrWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RejestrWPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TicTacToe_WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
